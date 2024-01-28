cf964f4e0b1906f65ec4d8323471b9b36b1d9cc5
Hypergang <157299373+Hypergang@users.noreply.github.com>
2024-01-28 21:08:25 +0700

    Update mm.h

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2273,7 +2273,8 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_READAHEAD_PAGES	0
+#define VM_MAX_READAHEAD	128	/* kbytes */
+#define VM_MIN_READAHEAD	8	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
